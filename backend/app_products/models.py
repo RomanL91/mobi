cb9030ed7544d374cf20decd178f032643b916a2
RomanL91 <dev_roman91@mail.ru>
2023-11-03 09:50:16 +0600

    первое представление модели продуктов

--- backend/app_products/models.py
+++ backend/app_products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Products(models.Model):
+    # категория - одна
+
+    # photo = photo - много
+    # color = color - один
+
+    name_product = models.CharField(verbose_name='Наименование продукта', max_length=150)
+    desc_product = models.TextField(verbose_name='Описание продукта', max_length=1000, blank=True)
+
+
+    display_price = models.BooleanField(verbose_name='Отображать цену', default=False)
+    price = models.DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2)
+
+    display_discount = models.BooleanField(verbose_name='Отобразить скидку', default=False)
+    discount = models.DecimalField(verbose_name='Скидка', max_digits=2, decimal_places=2)
+    # период действия скидки - отдельной сущностью со связью?
+
+    display_remaining_goods = models.BooleanField(verbose_name='Отобразить остаток товара', default=False)
+    remaining_goods = models.BigIntegerField(verbose_name='Остаток товара', default=0)
+
+    # показывать теги
+    # теги - много
+    
+    # показывать промо 
+    # промо - один
+
+    # показывать рейтинг 
+    # рейтингн - много
+
+    # показывать отзывы
+    # отзывы - много
+    
+    # показывать характеристики 
+    # характеристики - много
+
+    # слаг поле для индексации предзаполнение с имени
