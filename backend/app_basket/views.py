a1304772c0fc88a6996fc1e5f209fb3070893b94
RomanL91 <dev_roman91@mail.ru>
2023-11-08 12:54:08 +0600

    сериализация модели Корзины + перегрузка отображения

--- backend/app_basket/views.py
+++ backend/app_basket/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from app_basket.models import Basket
+
+from app_basket.serializers import BasketSerializer
+
+ 
+
+class BasketViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Basket.objects.all()
+    serializer_class = BasketSerializer
