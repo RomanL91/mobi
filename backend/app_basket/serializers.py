5f1cfdc9d9056cbb928dba5d12f9ef70d4bc02a3
RomanL91 <dev_roman91@mail.ru>
2023-11-09 17:31:50 +0600

    Basket

--- backend/app_basket/serializers.py
+++ backend/app_basket/serializers.py
@@ -9,9 +9,9 @@ class BasketSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        total_price_per_position = instance.product_cost
-        total_price_of_customer_cart = instance.total_cost
-        representation['total_price_per_position'] = total_price_per_position
-        representation['total_price_of_customer_cart'] = total_price_of_customer_cart
+        # total_price_per_position = instance.product_cost
+        # total_price_of_customer_cart = instance.total_cost
+        # representation['total_price_per_position'] = total_price_per_position
+        # representation['total_price_of_customer_cart'] = total_price_of_customer_cart
 
         return representation
--- backend/app_basket/views.py
+++ backend/app_basket/views.py
@@ -8,14 +8,39 @@ from rest_framework.response import Response
 
 from rest_framework.decorators import action
 
+from rest_framework import status
+
 
 class BasketViewSet(viewsets.ModelViewSet):
     queryset = Basket.objects.all()
     serializer_class = BasketSerializer
 
+    
+    def create(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(user_session=request.session.__dict__["_SessionBase__session_key"])
+        set_buy = set([el.products.pk for el in queryset])
+
+        if int(request.data['products']) in set_buy:
+            prod = queryset.get(products=int(request.data['products']))
+
+            self.lookup_field = 'pk'
+            self.kwargs['pk'] = prod.pk
+            
+            self.update(request)
+
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            return Response(serializer.data)
+        else:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
     @action(detail=False, methods=['get'])
     def visitor(self, request, pk=None):
-        queryset = Basket.objects.filter(user_session=request.session.__dict__["_SessionBase__session_key"])
+        queryset = self.get_queryset().filter(user_session=request.session.__dict__["_SessionBase__session_key"])
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
