44bc275e6e7b5ed15416ccb7413c16a6c5c4c3f8
RomanL91 <dev_roman91@mail.ru>
2023-11-10 14:34:19 +0600

    баг сщ связями при сериализации

--- backend/app_products/serializers.py
+++ backend/app_products/serializers.py
@@ -43,13 +43,8 @@ class ProductsSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         all_entity_image_product = instance.productimage_set.all()
-        all_entity_colors_product = instance.color_set.all()
         list_url_to_image = [
             image.image.url for image in all_entity_image_product
         ]
-        list_color_to_product = [
-            {'color_name': el.name_color, 'color_code': el.color} for el in all_entity_colors_product
-        ]
         representation['list_url_to_image'] = list_url_to_image
-        representation['all_colors_product'] = list_color_to_product
         return representation
