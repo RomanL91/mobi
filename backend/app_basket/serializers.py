af94b66397a40b7ca3e877e403e763c948211caf
RomanL91 <dev_roman91@mail.ru>
2023-11-22 09:42:59 +0600

    сделал хорошо фронту

--- backend/app_basket/serializers.py
+++ backend/app_basket/serializers.py
@@ -8,7 +8,6 @@ from app_products.serializers import ProductsSerializer
 
 
 class BasketSerializer(serializers.ModelSerializer):
-    products = ProductsSerializer()
     class Meta:
         model = Basket
         fields = "__all__"
@@ -22,11 +21,16 @@ class BasketSerializer(serializers.ModelSerializer):
                 products=instance['products']
             )
 
+        name_product = instance.products.name_product
+        product_images = [
+            i.image.url for i in instance.products.productimage_set.all()]
         price_per_prod = instance.products.price_with_discount_or_PROMO
         total_price_per_position = instance.product_cost
         total_price_of_customer_cart = instance.total_cost
         representation['price_per_prod'] = price_per_prod
         representation['total_price_per_position'] = total_price_per_position
         representation['total_price_of_customer_cart'] = total_price_of_customer_cart
+        representation['name_product'] = name_product
+        representation['product_images'] = product_images
 
         return representation
