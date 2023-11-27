644566fa2b3aae39e150e157b97d4352846f0e06
RomanL91 <dev_roman91@mail.ru>
2023-11-27 10:53:09 +0600

    Правки для корзины

--- backend/app_basket/serializers.py
+++ backend/app_basket/serializers.py
@@ -4,8 +4,6 @@ from rest_framework import serializers
 
 from app_basket.models import Basket
 
-from app_products.serializers import ProductsSerializer
-
 
 class BasketSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,13 +22,19 @@ class BasketSerializer(serializers.ModelSerializer):
         name_product = instance.products.name_product
         product_images = [
             i.image.url for i in instance.products.productimage_set.all()]
-        price_per_prod = instance.products.price_with_discount_or_PROMO
-        total_price_per_position = instance.product_cost
-        total_price_of_customer_cart = instance.total_cost
-        representation['price_per_prod'] = price_per_prod
-        representation['total_price_per_position'] = total_price_per_position
-        representation['total_price_of_customer_cart'] = total_price_of_customer_cart
+        
+        price_per_prod = instance.products.price
+        display_discount = instance.products.display_discount
+        if instance.products.display_discount:
+            price_per_prod -= price_per_prod * (instance.products.discount / 100)
+
         representation['name_product'] = name_product
         representation['product_images'] = product_images
+        representation['price_per_prod'] = price_per_prod
+        representation['display_discount'] = display_discount
+        representation['display_promo'] = instance.products.display_promo
+        if instance.products.display_promo:
+            representation['price_per_promo'] = instance.products.price_with_discount_or_PROMO
 
         return representation
+    
