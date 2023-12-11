365e3a4c72c113eab68d55003be568332dfc4f75
RomanL91 <dev_roman91@mail.ru>
2023-12-11 13:06:53 +0600

    кастомный виджет для отображения JSON поля в админке Django

--- backend/app_basket/admin.py
+++ backend/app_basket/admin.py
@@ -1,10 +1,35 @@
+import json
+
 from django.db import models
 from django.contrib import admin
 
 from app_basket.models import Basket, Order
 from app_products.models import Products
 
-from django_json_widget.widgets import JSONEditorWidget
+from django_json_widget.widgets import JSONEditorWidget # ЕСТЬ И ГОТОВЫЕ РЕШЕНИЯ для JSON
+from django.contrib.admin.widgets import AdminTextareaWidget
+from django.utils.html import format_html
+
+
+
+
+class CustomAdminJSONWidget(AdminTextareaWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        result = []
+        if name == 'order_product_list':
+            value_list = json.loads(value)
+            for el  in value_list:
+                p, q, t, pr = el.values()
+                result.extend(
+                    f'''
+                    Продукт: {p}\n\t\r<br>
+                    Колличество: {q}\n\t\r<br>
+                    Время добаления в корзину: {t}\n\t\r<br>
+                    Приминение ПРОМО: {pr}\n\t\r<br>
+                    {'='*80}\n\t\r<br>
+                    '''
+                )
+        return format_html("".join(result))
 
 
 @admin.register(Basket)
@@ -37,6 +62,6 @@ class OrderAdmin(admin.ModelAdmin):
         'user_session',
         'order_product_list'
     ]
-    # formfield_overrides = {
-    #     models.JSONField: {'widget': JSONEditorWidget}, # Или сделать кастомный виджет
-    # }
+    formfield_overrides = {
+        models.JSONField: {'widget': CustomAdminJSONWidget}, # Или сделать кастомный виджет ЕСТЬ И ГОТОВЫЕ РЕШЕНИЯ
+    }
